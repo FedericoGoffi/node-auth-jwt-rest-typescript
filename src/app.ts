284d7b288e3b2c397954de39d755689e52f9f12b
Goffi Federico <goffifederico123@gmail.com>
2024-11-15 09:06:09 -0300

    Adding cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,27 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
+import cors from 'cors';
 import authRoutes from './routes/authRoutes';
 import userRoutes from './routes/userRoutes';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
+const allowedOrigins = [ 'http://localhost:3000' ];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if(!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+}));
 
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
